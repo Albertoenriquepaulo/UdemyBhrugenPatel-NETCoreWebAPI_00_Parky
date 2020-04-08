ca55ed87f9eacebb6187163befe9d678645f7f73
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-04-08 15:16:24 +0200

    18. Get all National Parks

--- ParkyAPI/Controllers/NationalParksController.cs
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Model.DTOs;
 using ParkyAPI.Repository.IRepository;
 
 namespace ParkyAPI.Controllers
@@ -20,6 +21,21 @@ namespace ParkyAPI.Controllers
         {
             _npRepo = npRepo;
             _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetNationalParks()
+        {
+            var objList = _npRepo.GetNationalParks();
+
+            var objDTO = new List<NationalParkDTO>();
+            foreach (var item in objList)
+            {
+                objDTO.Add(_mapper.Map<NationalParkDTO>(item));
+            }
+
+
+            return Ok(objDTO);
         }
     }
 }
