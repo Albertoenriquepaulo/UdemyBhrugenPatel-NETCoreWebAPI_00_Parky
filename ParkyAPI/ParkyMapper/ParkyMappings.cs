975089f695e28385107b63aaa3eac936fa93b44d
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-04-08 14:52:48 +0200

    16. Auto Mapper Configuration

---
+++ ParkyAPI/ParkyMapper/ParkyMappings.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ParkyAPI.Model;
+using ParkyAPI.Model.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.ParkyMapper
+{
+    public class ParkyMappings : Profile
+    {
+        public ParkyMappings()
+        {
+            //ReverseMap -> to map both ways
+            CreateMap<NationalPark, NationalParkDTO>().ReverseMap();
+
+            //if we add more models we can add more mapping in here
+        }
+    }
+}
