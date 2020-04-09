9f7a0c5a6d957b5030831366c556c36d8de8e288
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-04-09 20:41:54 +0200

    64. Delete National Park (ALLCRUD COMPLETED)

--- ParkyWeb/Controllers/NationalParksController.cs
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -86,5 +86,16 @@ namespace ParkyWeb.Controllers
         {
             return Json(new { data = await _npRepo.GetAllAsync(SD.NationalParkAPIPath) });
         }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var status = await _npRepo.DeleteAsync(SD.NationalParkAPIPath, id);
+            if (status)
+            {
+                return Json(new { success = true, message = "Delete Successful" });
+            }
+            return Json(new { success = true, message = "Delete Not Successful" });
+        }
     }
 }
