4c9d3ba7a2b8566e6a0f7082378a22d0476a6409
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-04-10 20:27:30 +0200

    78. Add User Repository

---
+++ ParkyAPI/Repository/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using ParkyAPI.Data;
+using ParkyAPI.Model;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public User Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Register(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
